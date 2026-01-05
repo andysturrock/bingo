d42b1262ddf1b7c1b4c9927d92f10cc977fd1086
Andy Sturrock <8336404+andysturrock@users.noreply.github.com>
2026-01-05 17:26:22 +0000

    feat: deploy React app using AWS CDK with cross-region ACM

---
+++ infra/bin/infra.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
+import { CertificateStack } from '../lib/certificate-stack';
+import { InfraStack } from '../lib/infra-stack';
+
+const app = new cdk.App();
+
+const domainName = app.node.tryGetContext('domainName');
+if (!domainName) {
+  throw new Error('Please provide "domainName" via context, e.g., -c domainName=bingo.sturrock.org');
+}
+
+const certStack = new CertificateStack(app, 'BingoCertificateStack', {
+  env: { account: '636099490084', region: 'us-east-1' },
+  domainName,
+});
+
+new InfraStack(app, 'BingoInfraStack', {
+  env: { account: '636099490084', region: 'eu-west-2' },
+  domainName,
+});
